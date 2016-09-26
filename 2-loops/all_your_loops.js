603ca8dc0223d7d632d330dbb2125ea0f6c6200c
Andy Wolff <andy.wolff@gmail.com>
2016-09-26 16:04:21 -0500

    done

---
+++ 2-loops/all_your_loops.js
@@ -0,0 +1,18 @@
+/*
+1 - Write a for loop that runs 5 times. Each iteration will increase the
+    value of checkz by 1.
+*/
+var checkz = 0;
+for ( i = 0; i < 5; i++ ) {
+	checkz++;
+};
+
+/*
+2 - Write a while loop that runs 3 times. Each iteration will decrease
+    the value of checkz by 2
+*/
+var n = 0;
+while ( n < 3 ) {
+	checkz -= 2;
+	n++;
+}
